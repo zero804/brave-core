1a495675a896a7363d5e1b8ed25dc6ae461966de
Mario Sanchez Prada <mario@igalia.com>
2020-11-23 19:23:15 +0100

    [WIP] Migrate away from using ResourceIdentifier now that it's gone upstream
    As part of upstream bug 1134547[1], the ResourceIdentifier that Brave
    Shields relied on so far has been completely removed[2], so we need to
    adapt our code not to rely on it anymore so that we can keep building
    Brave correctly on the latest dev channel release of Chromium 88.
    
    This patch implements the first step to adapt Brave to this new
    scenario, by moving into using Brave-specific values of the
    ContentSettingsType enumeration instead of using the PLUGINS
    type plus a ResourceIdentifier as we did before.
    
    Note that this patch adds a "bs_" prefix to all the BraveShields
    constants, to avoid collisions with the COOKIES and JAVASCRIPT
    upstream content types. Added prefix to all constants for
    consistency + update TS code.
    
    Note that the goal of this change is simply to keep Brave building,
    running and passing the tests so that we can continue working on the
    rebase. Further work will be still needed in follow-up patches to
    fully migrate the code base to a post-Flash world by changing those
    bits that don't make sense anymore, and simplifying things as much
    as possible.
    
    [1] https://crbug.com/1134547
    [2] https://crrev.com/c/2500161

--- chromium_src/components/content_settings/core/browser/host_content_settings_map.cc
+++ chromium_src/components/content_settings/core/browser/host_content_settings_map.cc
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/content_settings/core/browser/brave_content_settings_ephemeral_provider.h"
 #include "brave/components/content_settings/core/browser/brave_content_settings_pref_provider.h"
 
 #define EphemeralProvider BraveEphemeralProvider
